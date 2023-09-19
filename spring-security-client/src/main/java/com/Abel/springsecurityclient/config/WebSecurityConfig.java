d40b95b49232c79e7ece8495ebdeb456357d5b11
Abel1512 <abelpriyan12@gmail.com>
2023-09-19 10:45:24 +0530

    SpringSecurityClientApplication

--- spring-security-client/src/main/java/com/Abel/springsecurityclient/config/WebSecurityConfig.java
+++ spring-security-client/src/main/java/com/Abel/springsecurityclient/config/WebSecurityConfig.java
@@ -2,15 +2,37 @@ package com.Abel.springsecurityclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private static final String[]WHITE_LIST_URLS={
+            "/register"
+    };
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(11);
     }
+
+   @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf((csrf) -> csrf
+                        .csrfTokenRepository(new HttpSessionCsrfTokenRepository()))
+                .authorizeRequests()
+                .requestMatchers(WHITE_LIST_URLS).permitAll();
+
+        return http.build();
+
+
+   }
+
 }
